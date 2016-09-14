caa5fd0df3e81a7fc9877ddb289d4b9756a4a8fe
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-09-14 18:32:42 +0200

    Refactor code examples for space, add links to website and github.

--- validation_fwork.py
+++ validation_fwork.py
@@ -4,7 +4,7 @@ datasets = {'GLDAS': {'class': gldas_reader, 'columns': ['086_L1']},
             'SMAP': {'class': smap_reader, 'columns': ['soil_moisture']},
             'ASCAT': {'class': ascat_reader, 'columns': ['sm']}}
 
-process = Validation(datasets, 'SMAP', {(3, 2): basic_metrics.calc_metrics,
-                                        (3, 3): metrics.triple_collocation},
-                     temporal_ref='ASCAT', scaling='cdf_match',
-                     scaling_ref='GLDAS')
+process = Validation(datasets, spatial_ref='SMAP', temporal_ref='ASCAT',
+                     metric_calculators={(3, 2): basic_metrics.calc_metrics,
+                                         (3, 3): metrics.triple_collocation},
+                     scaling_ref='GLDAS', scaling='cdf_match')
