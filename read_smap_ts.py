db034d409b9122ac57cc18c61d7959d21b6a4f0c
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-09-15 13:27:07 +0200

    Rephrase a few parts and fix typos.

--- read_smap_ts.py
+++ read_smap_ts.py
@@ -1,3 +1,3 @@
 from smap_io.interface import SMAPTs
-ds = SMAPTs('/data/smap_ts/', parameters='soil_moisture')
-ts = ds.read_ts(-73.96, 40.79)  # reading works by longitude and latitude
+smap_reader = SMAPTs('/data/smap_ts/', parameters='soil_moisture')
+ts = smap_reader.read_ts(-73.96, 40.79)  # reading works by longitude and latitude
